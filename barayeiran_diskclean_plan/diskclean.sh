79da26af905178d2f7806ceaa950f52eb6a8001c
hzamanian58 <hamid@gmail.com>
2023-07-30 11:25:47 +0330

    barayeiran_diskclean_plan/ is changed in find command

--- barayeiran_diskclean_plan/diskclean.sh
+++ barayeiran_diskclean_plan/diskclean.sh
@@ -7,34 +7,42 @@
 function log_message () {
 case $? in
         1)
-                echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) system coudn't remove $file " >> /home/barayeiran_diskclean_plan/diskclean.err
+                echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) system coudn't remove $file " >> /home/mobin_mobin_diskclean_plan/diskclean.err
                 ;;
         0)
-                echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) $file is removed successfuly" >> /home/barayeiran_diskclean_plan/diskclean.suc
+                echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) $file is removed successfuly" >> /home/mobin_diskclean_plan/diskclean.suc
                 ;;
         *)
-                echo "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) The error message was not detected" >> /home/barayeiran_diskclean_plan/diskclean.err
+                echo "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) The error message was not detected" >> /home/mobin_diskclean_plan/diskclean.err
                 exit 5
                 ;;
 esac
 }
 
-# When the disk consumption reaches more than 80%, the deletion of extra files will start.
+# When the disk consumption reaches more than 50%, the deletion of extra files will start.
 
 used=$(df -lh | egrep "/dev/sda3" | awk '{print $5}' | cut -f1 -d "%")
 
-if [ $used -le 70 ]; then
-        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) disk usage is: $used% and there was no need to run the diskclean program" >> /home/barayeiran_diskclean_plan/diskclean.suc
+if [ $used -lt 20 ]; then
+        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) disk usage is: $used% and there was no need to run the diskclean program" >> /home/mobin_diskclean_plan/diskclean.suc
 else
-        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) disk is very full and disk usage is: $used% ; then diskclean is run" >> /home/barayeiran_diskclean_plan/diskclean.err
-        my_find=$(find /home/barayeiran_backup -depth -iname "*.zip" -mtime +2 | sort)
-        my_array=($my_find)
-        for file in ${my_array[@]}
+        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) disk is very full and disk usage is: $used% ; therefore diskclean is run" >> /home/mobin_diskclean_plan/diskclean.err
+        FIND_ZIP=$(find /home/mobin_backup -depth -iname "*.zip" -mtime +7 | sort)
+        FIND_SQL=$(find /home/mobin_backup/sql_backup/ -depth -type d -mtime +7 | sort)
+
+
+        FIND_ALL=("${FIND_ZIP[@]}" "${FIND_SQL[@]}")
+#ALL_ARRAY=($FIND_ALL)
+
+
+
+       # ZIP_ARRAY=($FIND_ZIP)
+       # SQL_ARRAY=($FIND_SQL)
+        for file in ${FIND_ALL[@]}
         do
                 sudo rm -rvf $file || exit 1
                 log_message $file $?
         done
-	disk_space_reached=$(df -lh | egrep "/dev/sda3" | awk '{print $5}' | cut -f1 -d "%")
-        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) The diskclean program was executed and the disk space reached: $disk_space_reached% " >> /home/barayeiran_diskclean_plan/diskclean.suc
+        disk_space_reached=$(df -lh | egrep "/dev/sda3" | awk '{print $5}' | cut -f1 -d "%")
+        echo -e "$(date +"%a-%b-%d-%Y/%H:%M:%S") $(hostname) $(hostname -I) The diskclean program was executed and the disk space reached: $disk_space_reached% " >> /home/mobin_diskclean_plan/diskclean.suc
 fi
-
